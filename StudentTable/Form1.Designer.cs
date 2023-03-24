70f746dee54a48878080daf09cc40f3eec520336
getitwebthing <577641673@qq.com>
2023-03-25 01:27:33 +0800

    添加项目文件。

---
+++ StudentTable/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace StudentTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button Addbutton;
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstStudentIds = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.Class_nameBox = new System.Windows.Forms.TextBox();
+            this.genderBox = new System.Windows.Forms.TextBox();
+            this.ageBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.collegeBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            Addbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Addbutton
+            // 
+            Addbutton.Location = new System.Drawing.Point(22, 579);
+            Addbutton.Name = "Addbutton";
+            Addbutton.Size = new System.Drawing.Size(318, 61);
+            Addbutton.TabIndex = 16;
+            Addbutton.Text = "添加学生信息";
+            Addbutton.UseVisualStyleBackColor = true;
+            Addbutton.Click += new System.EventHandler(this.Addbutton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(645, 193);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(400, 582);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "查询同名学生";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(400, 357);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(227, 21);
+            this.txtName.TabIndex = 2;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(399, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "输入学生姓名查询学号";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lstStudentIds
+            // 
+            this.lstStudentIds.FormattingEnabled = true;
+            this.lstStudentIds.ItemHeight = 12;
+            this.lstStudentIds.Location = new System.Drawing.Point(401, 411);
+            this.lstStudentIds.Name = "lstStudentIds";
+            this.lstStudentIds.Size = new System.Drawing.Size(227, 124);
+            this.lstStudentIds.TabIndex = 4;
+            this.lstStudentIds.SelectedIndexChanged += new System.EventHandler(this.lstStudentIds_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(399, 392);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "学号";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(22, 358);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(318, 21);
+            this.nameBox.TabIndex = 6;
+            // 
+            // Class_nameBox
+            // 
+            this.Class_nameBox.Location = new System.Drawing.Point(22, 398);
+            this.Class_nameBox.Name = "Class_nameBox";
+            this.Class_nameBox.Size = new System.Drawing.Size(318, 21);
+            this.Class_nameBox.TabIndex = 7;
+            // 
+            // genderBox
+            // 
+            this.genderBox.Location = new System.Drawing.Point(22, 439);
+            this.genderBox.Name = "genderBox";
+            this.genderBox.Size = new System.Drawing.Size(318, 21);
+            this.genderBox.TabIndex = 8;
+            // 
+            // ageBox
+            // 
+            this.ageBox.Location = new System.Drawing.Point(22, 479);
+            this.ageBox.Name = "ageBox";
+            this.ageBox.Size = new System.Drawing.Size(318, 21);
+            this.ageBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 336);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "姓名";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 383);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "班级";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 424);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "性别";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 464);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "年龄";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 503);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "学院";
+            // 
+            // collegeBox
+            // 
+            this.collegeBox.Location = new System.Drawing.Point(22, 518);
+            this.collegeBox.Name = "collegeBox";
+            this.collegeBox.Size = new System.Drawing.Size(318, 21);
+            this.collegeBox.TabIndex = 15;
+            this.collegeBox.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(292, 32);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 12);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "学生数据表";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(141, 292);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 12);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "数据录入模块";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(473, 292);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 12);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "姓名查询学号模块";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 766);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(Addbutton);
+            this.Controls.Add(this.collegeBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ageBox);
+            this.Controls.Add(this.genderBox);
+            this.Controls.Add(this.Class_nameBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstStudentIds);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstStudentIds;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox Class_nameBox;
+        private System.Windows.Forms.TextBox genderBox;
+        private System.Windows.Forms.TextBox ageBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox collegeBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ StudentTable/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace StudentTable
+{
+    public partial class Form1 : Form
+    {
+        private static SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-PL29PE4;Initial Catalog=StudentDatabase;Integrated Security=True");
+        private static SqlDataAdapter adapter;
+        private static DataSet dataSet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // 创建数据适配器和数据集
+            adapter = new SqlDataAdapter("SELECT * FROM Students", connection);
+            dataSet = new DataSet();
+
+            // 填充数据集
+            adapter.Fill(dataSet);
+
+            // 将数据集绑定到DataGridView控件
+            dataGridView1.DataSource = dataSet.Tables[0];
+
+
+            // 设置 DataGridView 控件的位置和大小
+            //dataGridView1.Location = new Point(10, 10);
+            //dataGridView1.Size = new Size(500, 300);
+
+            // 将 DataGridView 控件添加到窗体中
+            Controls.Add(dataGridView1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            lstStudentIds.Items.Clear();
+
+            // 打开连接
+            connection.Open();
+
+            string query = "SELECT * FROM Students WHERE Name = @name";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@name", txtName.Text);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                lstStudentIds.Items.Add(reader["StudentID"].ToString());
+            }
+
+            // 关闭连接
+            connection.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {     
+        }
+
+        private void lstStudentIds_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Addbutton_Click(object sender, EventArgs e)
+        {
+
+
+            try {
+
+                string name = nameBox.Text;
+                string Class_name = Class_nameBox.Text;
+                string gender = genderBox.Text;
+                int age = int.Parse(ageBox.Text);
+                string college = collegeBox.Text;
+                string sql = "INSERT INTO Students(Name, Class, Gender, Age, College) VALUES (@name, @class, @gender, @age, @college)";
+
+
+                // 打开连接
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+
+                    // 添加参数
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Age", age);
+                    command.Parameters.AddWithValue("@Gender", gender);
+                    command.Parameters.AddWithValue("@College", college);
+                    command.Parameters.AddWithValue("@Class", Class_name);
+
+                    // 执行 SQL 语句并返回受影响的行数
+                    int affectedRows = command.ExecuteNonQuery();
+
+                    // 如果受影响的行数大于 0，则添加数据成功
+                    if (affectedRows > 0)
+                    {
+                        MessageBox.Show("添加数据成功");
+                    }
+                    else
+                    {
+                        MessageBox.Show("添加数据失败");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("添加数据失败");
+            }
+
+         
+
+            //刷新DataGridView 控件数据
+            {
+                // 创建数据适配器和数据集
+                adapter = new SqlDataAdapter("SELECT * FROM Students", connection);
+                dataSet = new DataSet();
+
+                // 填充数据集
+                adapter.Fill(dataSet);
+
+                // 将数据集绑定到DataGridView控件
+                dataGridView1.DataSource = dataSet.Tables[0];
+
+                // 将 DataGridView 控件添加到窗体中
+                Controls.Add(dataGridView1);
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
